2e0e155d8a6a3368ae00638fbb687daa0c7ada22
Dakota Martinez <dakotaleemusic@gmail.com>
2016-04-13 23:36:07 +0000

    completes cart model spec

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,20 @@
 class Cart < ActiveRecord::Base
-
+  has_many :line_items
+  has_many :items, through: :line_items
+  
+  def total 
+    line_items.collect do |line_item|
+      line_item.amount
+    end.inject(0, :+)
+  end
+  
+  def add_item(item_id)
+    line_item = line_items.find_by(id: item_id)
+    if line_item 
+      line_item.quantity += 1
+      line_item
+    else 
+      line_items.build(quantity: 1, item_id: item_id)
+    end
+  end
 end
--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,3 +1,12 @@
 class LineItem < ActiveRecord::Base
-
+  belongs_to :cart
+  belongs_to :item
+  
+  def unit_price 
+    self.item.price
+  end
+  
+  def amount 
+    quantity * unit_price
+  end
 end
